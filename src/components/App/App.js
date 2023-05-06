703f3c8a385d72082bb82629598d8ee0f599bc9e
Serhi Hats <gatsserv@gmail.com>
2023-05-06 16:41:52 +0300

    init for third task

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,15 @@ import {Component} from "react";
 import Header from "../Header/Header";
 import FormFeedback from "../FormFeedback/FormFeedback";
 import ParagraphRequest from "../ParagraphRequest/ParagraphRequest";
+import ComponentConvert from "../ComponentConvert/ComponentConvert";
 
 class App extends Component {
 
   state = {
     task1: "React: Домашнє завдання 5. Завдання 2",
+    task2: "React: Домашнє завдання 5. Завдання 3",
     description1: "Створити компонент форми з трьома контрольованими вводами. Після відправки форми – сторінка не перезавантажується, а вміст полів виводиться у вигляді json під формою (json-об'єкт потрібно перевести в рядок).",
+    description2: "Створіть компонент із двома контрольованими компонентами, які рендерять. Вони повинні приймати тільки числа. Після того, як користувач вводить число до input, його вміст виводиться в блоці під ним з конвертацією - number * 0,5 та number * 0,05 відповідно.",
     requestForm: false,
   }
 
@@ -25,6 +28,8 @@ class App extends Component {
           this.handlerForm(obj)
         }}/>
         <ParagraphRequest requestForm={this.state.requestForm}/>
+        <Header nameTask={this.state.task2} descriptionTask={this.state.description2}/>
+        <ComponentConvert/>
 
       </div>
     );
---
+++ src/components/ComponentConvert/ComponentConvert.js
@@ -0,0 +1,14 @@
+import "./ComponentConvert.css";
+import {Component} from "react";
+
+class ComponentConvert extends Component {
+  render() {
+    return (
+      <div className={"ComponentConvert"}>
+        someThink
+      </div>
+    );
+  }
+}
+
+export default ComponentConvert;
